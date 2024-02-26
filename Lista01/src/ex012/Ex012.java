4629ea1be6f941e3bfa08373749a0816651f1d65
Juliano Almada <julianoalmada48@gmail.com>
2024-02-26 19:59:29 -0300

    refatoração do exercício 12 e exercício em sala

--- Lista01/src/ex012/Ex012.java
+++ Lista01/src/ex012/Ex012.java
@@ -1,11 +1,20 @@
 package ex012;
+
+import java.util.Scanner;
+
 public class Ex012 {
     public static void main(String[] args) {
-        int soma1 = 0, soma2=0;
-        for (int i = 1; i <=10; i++){
-            soma1 += (int) Math.pow(i, 2);
-            soma2 += i;
-        }
-        System.out.printf("Soma dos quadrados: %d\nQuadrado da soma: %.0f\nDiferença: %.0f", soma1, Math.pow(soma2,2),(Math.pow(soma2,2) - soma1));
+        Scanner s = new Scanner(System.in);
+        double soma1 = 0, soma2 = 0;
+        System.out.println("Insira o limite da operação: ");
+        int n = s.nextInt();
+
+        do{
+            soma1 += Math.pow(n, 2);
+            soma2 += n;
+            n--;
+        }while (n>=1);
+
+        System.out.printf("Soma dos quadrados: %.0f\nQuadrado da soma: %.0f\nDiferença: %.0f", soma1, Math.pow(soma2,2),(Math.pow(soma2,2) - soma1));
     }
 }
