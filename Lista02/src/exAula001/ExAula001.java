2a7fe51923c038a3e9af3bc8d218f7bd55f09606
Juliano Almada <julianoalmada48@gmail.com>
2024-02-28 19:56:33 -0300

    exercicios em aula002

--- Lista02/src/exAula001/ExAula001.java
+++ Lista02/src/exAula001/ExAula001.java
@@ -6,8 +6,6 @@ public class ExAula001 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        System.out.println("Digite o seu peso: ");
-        double peso = s.nextDouble();
         System.out.println("Digite a sua altura: ");
         double altura = s.nextDouble();
         System.out.println("Digite o seu sexo: ");
