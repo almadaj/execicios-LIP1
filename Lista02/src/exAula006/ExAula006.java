1116e5b3da5eaf2e8b93495a903c74c2a8d787a8
Juliano Almada <julianoalmada48@gmail.com>
2024-03-13 19:54:24 -0300

    aula de manipulação de arquivos

--- Lista02/src/exAula006/ExAula006.java
+++ Lista02/src/exAula006/ExAula006.java
@@ -1,18 +1,32 @@
 package exAula006;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class ExAula006 {
+    public static void writerFile(String filename, String message){
+        try{
+            FileWriter fw = new FileWriter(filename);
+            fw.write(message);
+            fw.close();
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void readerFile(String filename){
+
+    }
     public static void main(String[] args) {
-        File myfile = new File("arquivo.txt");
+        File file = new File("Teste.txt");
         Scanner s = new Scanner(System.in);
-        File file;
-        String filename;
-        System.out.println("Digite o nome do arquivo: ");
-        filename = s.next();
-        file = new File(filename.concat(".txt"));
+
+//        String filename;
+//        System.out.println("Digite o nome do arquivo: ");
+//        filename = s.next();
+//        file = new File(filename.concat(".txt"));
 
         try {
             if (file.createNewFile()) {
@@ -21,6 +35,10 @@ public class ExAula006 {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+
+        System.out.println("Digite a mensagem: ");
+        String message = s.nextLine();
+        writerFile("Teste.txt", message);
     }
 }
 
