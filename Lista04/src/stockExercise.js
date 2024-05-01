dd51003954b965153e18e57a94051d54993aa0ac
Juliano Almada <almadaj@edu.unifor.br>
2024-05-01 12:15:43 -0300

    refatoração do map

--- Lista04/src/stockExercise.js
+++ Lista04/src/stockExercise.js
@@ -1,27 +1,31 @@
-let prompt = require('prompt-sync')()
-let products = []
+let prompt = require("prompt-sync")();
+let products = [];
 
-for(let i = 0; i < 3; i++){
-    var product = {}
-    product.name = prompt("Insert product's name: ")
-    product.price = prompt("Insert price: ")
-    product.qtd = prompt("Insert quantity: ")
+for (let i = 0; i < 3; i++) {
+  var product = {};
+  product.name = prompt("Insert product's name: ");
+  product.price = prompt("Insert price: ");
+  product.qtd = prompt("Insert quantity: ");
 
-    products.push(product)
+  products.push(product);
 }
 
 function verifyStock(name) {
-    let found = false;
-    for (let i = 0; i < products.length; i++) {
-        if (name === products[i].name) {
-            console.log(`${products[i].name} has ${products[i].qtd} units`);
-            found = true;
-            break;
-        }
+  let found = false;
+  for (let i = 0; i < products.length; i++) {
+    if (name === products[i].name) {
+      console.log(`${products[i].name} has ${products[i].qtd} units`);
+      found = true;
+      break;
     }
-    if (!found) {
-        console.log(`${name} not found!`);
-    }
+  }
+  if (!found) {
+    console.log(`${name} not found!`);
+  }
 }
 
-console.log(products)
+verifyStock("Pasta");
+
+products.map((product) => {
+  console.log(product);
+});
