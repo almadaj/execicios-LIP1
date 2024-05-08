519464792f4082119535a56bcf4883138714781f
Juliano Almada <julianoalmada48@gmail.com>
2024-05-08 19:09:06 -0300

    exercicio com reduce

--- Lista04/src/methodsArray.js
+++ Lista04/src/methodsArray.js
@@ -21,3 +21,23 @@ function mapping(temp) {
 
 let converted = nums.map(mapping);
 console.log(converted);
+
+
+let arr = [1, 2, 3, 4, 5]
+
+let result = arr.reduce((sum, current) => sum + current, 0)
+
+console.log(result) //15
+
+
+let ask = [12, 9, 28, 15, 13, 26]
+
+let res = ask.reduce(function(biggest, current){
+  if(current > biggest){
+    return current
+  }else{
+    return biggest
+  } 
+}, ask[0])
+
+console.log(res)
